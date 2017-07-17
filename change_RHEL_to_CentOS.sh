0dd545c152bb74672d6a8c975bf109b08be254bf
jackeichen <gao_ysu@163.com>
2017-07-17 12:45:43 +0800

    Add files via upload

---
+++ change_RHEL_to_CentOS.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#This is for RHEL 7.2, that cannot use yum tool. Modify freely if not RHEL 7.2
+#Created by Gaozh.
+#given is 163 source.
+
+echo "Environment checking..."
+jud1=`whoami`
+if [ $jud1 != "root" ]
+then
+  echo "Please use it with root user"
+  exit 1
+else
+  echo "User: root    Check done"
+fi
+
+ping www.baidu.com -c 2 > /dev/null 2>&1
+if [ $? -ne 0 ]
+then 
+  echo "Please check your net connect"
+  exit 2
+else
+  echo "Net: OK    Check done"
+fi
+echo "Environment check done"
+
+#rmove OS yum tool,which we cannot use
+rpm -qa | grep "yum"
+if [ $? -eq 0 ]
+then
+  rpm -qa | grep yum | xargs rpm -e --nodeps
+  rpm -qa | grep yum
+  if [ $? -eq 0 ]
+  then
+    echo "fail to rmove yum, do it manualy."
+    exit 3
+  fi
+fi
+
+#install CentOS yum tool
+rpm -ivh ./yum-*.rpm
+if [ $? -ne 0 ]
+then
+  echo "fail to install yum."
+  exit 4
+fi
+
+#create CentOS .repo file
+cp -r /etc/yum.repos.d /etc/yum.repos.d.bak
+rm -rf /etc/yum.repos.d/*.repo
+cp ./CentOS-Base.repo /etc/yum.repos.d/
+
+# make cache
+yum clean all
+yum makecache
+
+
